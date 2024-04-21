02be106ce23ce6d37323dd564cd672968cafc1fa
Tenarien <mamateki7@gmail.com>
2024-04-21 19:44:04 +0100

    Add GenresClient to retrieve genres

---
+++ GameStore.Frontend/Clients/GenresClient.cs
@@ -0,0 +1,32 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GenresClient
+{
+    private readonly Genre[] genres = 
+    [
+        new () {
+            Id = 1,
+            Name = "Fighting",
+        },
+        new () {
+            Id = 2,
+            Name = "RPG",
+        },
+        new () {
+            Id = 3,
+            Name = "Racing",
+        },
+        new () {
+            Id = 4,
+            Name = "Shooter",
+        },
+        new () {
+            Id = 5,
+            Name = "Sports",
+        },
+    ];
+
+    public Genre[] GetGenres() => genres;
+}
