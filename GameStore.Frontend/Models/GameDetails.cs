a9469eecdd5b7bff7993132a830c6a75f466ca8b
Tenarien <mamateki7@gmail.com>
2024-04-21 19:54:27 +0100

    Fix typos

--- GameStore.Frontend/Models/GameDetails.cs
+++ GameStore.Frontend/Models/GameDetails.cs
@@ -2,7 +2,7 @@
 
 public class GameDetails
 {
-    public int ID { get; set; }
+    public int Id { get; set; }
     public required string Name { get; set; }
     public string? GenreId { get; set; }
     public decimal Price { get; set; }
