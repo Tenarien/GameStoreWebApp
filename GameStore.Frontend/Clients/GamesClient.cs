765ede19a4c796e16e0c159daee7da92661d5813
Tenarien <mamateki7@gmail.com>
2024-04-21 20:20:29 +0100

    Add function to add a new game

--- GameStore.Frontend/Clients/GamesClient.cs
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -7,28 +7,28 @@ public class GamesClient
     private readonly List<GameSummary> games = 
     [
         new() {
-            ID = 1,
+            Id = 1,
             Name = "Starfield",
             Genre = "RPG",
             Price = 59.99M,
             ReleaseDate = new DateOnly(2024, 2, 10)
         },
         new() {
-            ID = 2,
+            Id = 2,
             Name = "Witcher 3",
             Genre = "RPG",
             Price = 29.99M,
             ReleaseDate = new DateOnly(2012, 3, 15)
         },
         new() {
-            ID = 3,
+            Id = 3,
             Name = "Forza Horizon 5",
             Genre = "Racing",
             Price = 59.99M,
             ReleaseDate = new DateOnly(2023, 1, 24)
         },
         new() {
-            ID = 4,
+            Id = 4,
             Name = "Call Of Duty: Modern Warfare",
             Genre = "Shooter",
             Price = 59.99M,
@@ -36,5 +36,24 @@ public class GamesClient
         },
     ];
 
+    private readonly Genre[] genres = new GenresClient().GetGenres();
+
     public GameSummary[] GetGames() => [.. games];
+
+    public void AddGame(GameDetails game)
+    {   
+        ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
+        var genre = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
+
+        var gameSummary = new GameSummary
+        {
+            Id = games.Count + 1,
+            Name = game.Name,
+            Genre = genre.Name,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+
+        games.Add(gameSummary);
+    }
 }
